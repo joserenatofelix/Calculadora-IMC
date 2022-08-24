870b6aa3bd1b0c43818e9bfad3210b7bbbcddbbf
José Renato Felix da Silva <93296913+joserenatofelix@users.noreply.github.com>
2022-08-24 16:06:52 -0300

    update_3

--- funções.js
+++ funções.js
@@ -35,14 +35,60 @@ function go(currentStep,nextStep)
 
 function validate()
 {
-    const peso = document.getElementById('peso');
+    const peso   = document.getElementById('peso');
     const altura = document.getElementById('altura');
-    if(!peso || !altura)
+    peso.style.border   = 'none';
+    altura.style.border = 'none';
+    if(!peso.value || !altura.value)
     {
-        console.log('campos incorretos');
+        if(!peso.value && !altura.value)
+        {
+            peso.style.border = '1px solid red';
+            altura.style.border = '1px solid red';
+        }
+        else if(!peso.value)
+        {
+            peso.style.border = '1px solid red';
+        }
+        else
+        {
+            altura.style.border = '1px solid red';
+        }
     }
     else
     {
-        console.log('tudo ok! calcular');
+        let imc = peso.value / (altura.value * altura.value);
+        const result = document.getElementById('resultado');
+        if(imc < 18.5)
+        {
+            console.log('Magreza | Obesidade: 0');
+            result.style.color = 'yellow';
+            result.innerHTML = 'Magreza | Obesidade: 0';
+        }
+        else if(imc >= 18 && imc < 25)
+        {
+            console.log('Normal | Obesidade: 0');
+            result.style.color = 'green';
+            result.innerHTML = 'Normal | Obesidade: 0';
+        }
+        else if(imc >= 25 && imc < 30)
+        {
+            console.log('Sobrepeso | Obesidade: l');
+            result.style.color = 'orange';
+            result.innerHTML = 'Sobrepeso | Obesidade: l';
+        }
+        else if(imc >= 30 && imc < 40)
+        {
+            console.log('Obesidade | Obesidade: ll');
+            result.style.color = 'red';
+            result.innerHTML = 'Obesidade | Obesidade: ll';
+        }
+        else
+        {
+            console.log('Obesidade grave| Obesidade: lll');
+            result.style.color = 'black';
+            result.innerHTML = 'Obesidade grave| Obesidade: lll';
+        }
+        go(1,2);
     }
 }
